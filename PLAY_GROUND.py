1b70cb5ed5cd20f4d8955514d08baed84fe6a893
MaratMedvedev <90756690+MaratMedvedev@users.noreply.github.com>
2023-05-08 10:45:10 +0300

    Create PLAY_GROUND.py

---
+++ PLAY_GROUND.py
@@ -0,0 +1,24 @@
+from typing import List
+
+import gym as gym
+from GA import GA
+from MODEL_CNN import ModelCNN
+
+def load_weights_from_file(name) -> List[float]:
+    with open(name, "r") as f:
+        return [float(weight) for weight in f.readlines()]
+
+
+
+env = gym.make('ALE/AirRaid-v5',
+               obs_type="grayscale",
+               render_mode='human',
+               frameskip=10,
+               repeat_action_probability=0)
+env.metadata['video.frames_per_second'] = 120
+
+MODEL = ModelCNN()
+
+best_weights = load_weights_from_file("best_weights_2050.0.txt")
+total_reward = MODEL.play(env, best_weights)
+print(f"Total reward: {total_reward}")
