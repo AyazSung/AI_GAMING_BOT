ec5271c6c105f814d05f4b8fbbdc5b4bf18f922a
MaratMedvedev <90756690+MaratMedvedev@users.noreply.github.com>
2023-05-08 11:24:10 +0300

    Update PLAY_GROUND.py

--- PLAY_GROUND.py
+++ PLAY_GROUND.py
@@ -13,12 +13,12 @@ def load_weights_from_file(name) -> List[float]:
 env = gym.make('ALE/AirRaid-v5',
                obs_type="grayscale",
                render_mode='human',
-               frameskip=10,
+               frameskip=7,
                repeat_action_probability=0)
 env.metadata['video.frames_per_second'] = 120
 
 MODEL = ModelCNN()
 
-best_weights = load_weights_from_file("best_weights_2050.0.txt")
+best_weights = load_weights_from_file("best_weights_1425.txt")
 total_reward = MODEL.play(env, best_weights)
 print(f"Total reward: {total_reward}")
